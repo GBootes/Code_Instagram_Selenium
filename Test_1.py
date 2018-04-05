b502b2b1e67d4872411c7a547461cf457ed67adc
Johny Alexander Vallejo Sánchez <jhonyal58@gmail.com>
2018-04-05 12:29:34 -0500

    Users in a External File
    Se guarda los nombres de usuario de cada publicación en un archivo externo .txt

--- Test_1.py
+++ Test_1.py
@@ -9,47 +9,70 @@ def Time(t):
     for i in range(0,t):
 
         a=i
+        
+def Data_Out(file,x):
+    
+    DataOut=open(file,'a')
+    DataOut.write(x)
+    DataOut.close
+    
 
 DataLog=open('DataLog.txt','r')
 user=DataLog.readline()
 passw=DataLog.readline()
+url=DataLog.readline()
+t1=DataLog.readline()
+t2=DataLog.readline()
    
 driver=webdriver.Chrome("C:\Selenium\chromedriver.exe")
-driver.get("https://www.instagram.com/accounts/login/")
+driver.get(url)
 driver.maximize_window()
 
-driver.find_element_by_name("username").send_keys(user)
-ps=driver.find_element_by_name("password").send_keys(passw)
+userField=driver.find_element_by_name("username").send_keys(user)
+passField=driver.find_element_by_name("password").send_keys(passw)
 driver.implicitly_wait(10)
-driver.find_element_by_xpath("//span[button/@class='_qv64e _gexxb _4tgw8 _njrw0']").click()
+logBtn=driver.find_element_by_xpath("//span[button/@class='_qv64e _gexxb _4tgw8 _njrw0']").click()
+#driver.find_element_by_name("username").send_keys(user)
+#driver.find_element_by_name("password").send_keys(passw)
+#driver.implicitly_wait(10)
+#driver.find_element_by_xpath("//span[button/@class='_qv64e _gexxb _4tgw8 _njrw0']").click()
 
-driver.find_element_by_xpath("//div[@class='_5ayw3 _ohiyl']/input[1]").send_keys('#tenisAAA')
+driver.find_element_by_xpath("//div[@class='_5ayw3 _ohiyl']/input[1]").send_keys('#camiloferrer')
 driver.find_element_by_xpath("//a[@class='_ndl3t']/div[1]").click()
-
-driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]").click()
 
 #Número de Post
 numPost=eval(driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/header/div/span/span").get_attribute('innerHTML'))
+
+driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]").click()
 
 #Lista nombre de usuarios
 userList=[]
 users=[]
 
-for i in range(1,numPost+1):
+for i in range(numPost):
 
-    if(i==1 or i==numPost):
+    if(i==0):
 
-        Time(int(2.5e7))
         #WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div/div/a"))).click()
-        driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a").click()
         userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
+        Time(int(eval(t1)))
+        driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a").click()
+        Time(int(eval(t2)))
+        Data_Out('Users.txt',userList[i])
 
+    elif(i==numPost-1):
+
+        userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
+        Data_Out('Users.txt',userList[i])
+        
     else:
 
-        Time(int(2.5e7))
         #WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div/div/a[2]"))).click()
-        driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a[2]").click()
         userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
+        Time(int(eval(t1)))
+        driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a[2]").click()
+        Time(int(eval(t2)))
+        Data_Out('Users.txt',userList[i])
 
 users.append(userList[0])
 
