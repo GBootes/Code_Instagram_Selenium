5ca3cf0bc209767721a40a4d0386a6e9fba4e6ac
Johny Alexander Vallejo Sánchez <jhonyal58@gmail.com>
2018-04-05 22:16:09 -0500

    Search User and Save an external File
    Se busca publicaciones y se guarda cada usuario en un archivo externo.

--- Test_1.py
+++ Test_1.py
@@ -14,8 +14,29 @@ def Data_Out(file,x):
     
     DataOut=open(file,'a')
     DataOut.write(x)
+    DataOut.write('\n')
     DataOut.close
+
+def User_Repeat(userList,users,file2):
+
+    for i in userList:
     
+        s=True
+        for k in users:
+
+            if (k==i):
+
+                s=False
+
+        if (s):
+
+            users.append(i)
+
+    for i in users:
+
+        Data_Out(file2,i)
+
+    return users
 
 DataLog=open('DataLog.txt','r')
 user=DataLog.readline()
@@ -23,68 +44,63 @@ passw=DataLog.readline()
 url=DataLog.readline()
 t1=DataLog.readline()
 t2=DataLog.readline()
-   
-driver=webdriver.Chrome("C:\Selenium\chromedriver.exe")
-driver.get(url)
-driver.maximize_window()
 
-userField=driver.find_element_by_name("username").send_keys(user)
-passField=driver.find_element_by_name("password").send_keys(passw)
-driver.implicitly_wait(10)
-logBtn=driver.find_element_by_xpath("//span[button/@class='_qv64e _gexxb _4tgw8 _njrw0']").click()
-#driver.find_element_by_name("username").send_keys(user)
-#driver.find_element_by_name("password").send_keys(passw)
-#driver.implicitly_wait(10)
-#driver.find_element_by_xpath("//span[button/@class='_qv64e _gexxb _4tgw8 _njrw0']").click()
-
-driver.find_element_by_xpath("//div[@class='_5ayw3 _ohiyl']/input[1]").send_keys('#camiloferrer')
-driver.find_element_by_xpath("//a[@class='_ndl3t']/div[1]").click()
-
-#Número de Post
-numPost=eval(driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/header/div/span/span").get_attribute('innerHTML'))
-
-driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]").click()
-
-#Lista nombre de usuarios
-userList=[]
-users=[]
-
-for i in range(numPost):
-
-    if(i==0):
-
-        #WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div/div/a"))).click()
-        userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
-        Time(int(eval(t1)))
-        driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a").click()
-        Time(int(eval(t2)))
-        Data_Out('Users.txt',userList[i])
-
-    elif(i==numPost-1):
-
-        userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
-        Data_Out('Users.txt',userList[i])
-        
-    else:
-
-        #WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div/div/a[2]"))).click()
-        userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
-        Time(int(eval(t1)))
-        driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a[2]").click()
-        Time(int(eval(t2)))
-        Data_Out('Users.txt',userList[i])
-
-users.append(userList[0])
-
-for i in userList:
+def Search_Profile(user,passw,url,t1,t2,file1,file2,HT):
     
-    s=True
-    for k in users:
+    driver=webdriver.Chrome("C:\Selenium\chromedriver.exe")
+    driver.get(url)
+    driver.maximize_window()
 
-        if (k==i):
+    userField=driver.find_element_by_name("username").send_keys(user)
+    passField=driver.find_element_by_name("password").send_keys(passw)
+    driver.implicitly_wait(10)
+    logBtn=driver.find_element_by_xpath("//span[button/@class='_qv64e _gexxb _4tgw8 _njrw0']").click()
 
-            s=False
+    driver.find_element_by_xpath("//div[@class='_5ayw3 _ohiyl']/input[1]").send_keys(HT)
+    driver.find_element_by_xpath("//a[@class='_ndl3t']/div[1]").click()
 
-    if (s):
+    #Número de Post
+    numPost=eval(driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/header/div/span/span").get_attribute('innerHTML'))
 
-        users.append(i)
+    driver.find_element_by_xpath("//*[@id='react-root']/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]").click()
+
+    #Lista nombre de usuarios
+    userList=[]
+    users=[]
+
+    for i in range(numPost):
+
+        if(i==0):
+
+            #WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div/div/a"))).click()
+            userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
+            Time(int(eval(t1)))
+            driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a").click()
+            Time(int(eval(t2)))
+            Data_Out(file1,userList[i])
+
+        elif(i==numPost-1):
+
+            userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
+            Data_Out(file1,userList[i])
+        
+        else:
+
+            #WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div[1]/div/div/a[2]"))).click()
+            userList.append(driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div/article/header/div[2]/div[1]/div[1]/a").get_attribute('title'))
+            Time(int(eval(t1)))
+            driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/a[2]").click()
+            Time(int(eval(t2)))
+            Data_Out(file1,userList[i])
+
+    driver.quit()
+
+    users.append(userList[0])
+
+    users=User_Repeat(userList,users,file2)
+
+    return users
+
+profiles=[]
+#User,#Password,#URL,#Time_1,#Time_2,#Archive all users,#Archive users,#HashTag
+profiles=Search_Profile(user,passw,url,t1,t2,'Test1.txt','Test2.txt','#camiloferrer')
